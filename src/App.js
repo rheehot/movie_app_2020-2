e1f70d2877620fd73831c180a54e753a6265d44d
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2020-11-18 00:29:05 +0900

    3.0

--- src/App.js
+++ src/App.js
@@ -1,65 +1,22 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+class App extends React.Component{
+  state = {
+    count: 0
+  }
+  add = () => {
+   alert(1);
+  }
+  minus = () => {
 
-const foodIlLike = [
-  {
-    id: 1,
-    name: "coffee",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2zEvsWGOjghcpdwRt2gn9AHaEK%26pid%3DApi&f=1",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "pork",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.AavXF-AGGev29OHC0733KQHaLH%26pid%3DApi&f=1",
-    rating: 4.8,
-  },
-  {
-    id: 3,
-    name: "gogi",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.klL2I17BuTsNjoHAPPemnAHaEn%26pid%3DApi&f=1",
-    rating: 4.5,
-  },
-  {
-    id: 4,
-    name: "kimbab",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.ay3kZ7klIDPbNrS8dc4GXAHaEe%26pid%3DApi&f=1",
-    rating: 4.1,
-  },
-  {
-    id: 5,
-    name: "kimchi",
-    image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffoodtoglow.files.wordpress.com%2F2016%2F10%2Fkimchi1.jpg%3Fw%3D1200&f=1&nofb=1",
-    rating: 4.3,
-  },
-]
-
-
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} />
-    </div>
-  )
+  }
+ render() {
+ return <div>
+   <h1>The number is {this.state.count}</h1>
+   <button onClick={this.add}>Add</button>
+   <button onClick={this.minus}>Minus</button>
+  </div>
+ }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function App() {
-  return (
-    <div>
-      {foodIlLike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  );
-}
-
 export default App;
